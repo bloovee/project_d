099d1e8b68fe0cfa943b55d7b101cdf227bbeb16
bloovee <bloovee2021@gmail.com>
2023-04-26 20:38:45 +0800

    Deployment

--- config/settings/development.py
+++ config/settings/development.py
@@ -18,3 +18,14 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Gmail
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+# EMAIL_PORT = 465
+# EMAIL_USE_TLS = False
+# EMAIL_USE_SSL = True
+# DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+
